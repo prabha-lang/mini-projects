4faecd86cf3367a0bea0ce64bddd04db2dc14052
prabha-lang <prabhakarankc333@gmail.com>
2026-07-30 18:35:54 +0530

    Add files via upload

---
+++ meth.cs
@@ -0,0 +1,11 @@
+//int firstValue = 500;
+//int secondValue = 600;
+//int largerValue;
+
+//Console.WriteLine(largerValue);
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue , secondValue);
+Console.WriteLine(largerValue);
