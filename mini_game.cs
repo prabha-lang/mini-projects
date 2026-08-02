608d5ff1f9884d258bf85cbf79d596db19e83012
prabha-lang <prabhakarankc333@gmail.com>
2026-08-02 09:44:18 +0530

    Enhance game logic with new prize conditions
    Added new prize conditions based on total roll values and improved output messages.

--- mini_game.cs
+++ mini_game.cs
@@ -1,3 +1,4 @@
+
 Random dice = new();
 
 int roll1 = dice.Next(1 , 7);
@@ -5,14 +6,35 @@ int roll2 = dice.Next(1 , 7);
 int roll3 = dice.Next(1 , 7);
 int Total = roll1 + roll2 + roll3;
 
-Console.WriteLine($" you rolled a {roll1}, a {roll2}, and a {roll3}, for total {Total} ");
-
-if (Total > 14)
+Console.WriteLine($" you rolled a = {roll1}, b = {roll2}, and c = {roll3}, for total = {Total} ");
+if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
 {
-    Console.WriteLine("you hit  da jackpot");
+   if  ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("you rolled a triple plus 3 points for you");
+        Total  += 3;
+    }
+    else
+    {
+        Console.WriteLine("you rolled a double plus 2 points for you");
+        Total += 2;
+    }
 }
-if (Total < 15)
-{                              
-    
-    Console.WriteLine("you lost bitch");
+// ahh idhukula inoru else statement create panum
+if (Total >= 16)
+{
+    Console.WriteLine("You win a watermelon!");
 }
+else if ( Total >= 10)
+{                                
+    Console.WriteLine("you win a mobile stand!");
+} 
+else if (Total == 7)
+{
+    Console.WriteLine("you win a carpet!");
+}
+else
+{
+    Console.WriteLine(" you  lost bitch!");
+}
+ 
